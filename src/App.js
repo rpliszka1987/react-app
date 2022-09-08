dcea44c2af7562503207547230498e8392c82d6f
Robert Pliszka <robplisz@gmail.com>
2022-09-08 15:51:52 -0400

    Added useState() and useEffect() to the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// Import useState and useEffect from react library
+import { useState, useEffect } from "react";
 
-function App() {
+// This is array destructing. Which will assign a value to each of the array name in the [ ]
+// const [firstCity, seconCity] = ["Tokyo", "Tahoe City", "Bend"];
+
+// console.log(firstCity, seconCity);
+
+// Creating component with a destructuring the props. Helps avoid using props all the time to call the object property
+function App({ library }) {
+  // Setting the state of the emotion with initial value of happy. 
+  const [emotion, setEmotion] = useState("happy");
+
+  // Happens along side of render with the changes to the emothion array
+  useEffect(() => {
+    console.log(`It's ${emotion} right now!`)
+  }, [emotion])
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Current emotion is {emotion}</h1>
+      {/* Changes the state of the emotion */}
+      <button onClick={() => setEmotion('Sad')}>Sad</button>
+      <button onClick={() => setEmotion('Excited')}>Excited</button>
     </div>
   );
 }
 
+// Export the component
 export default App;
