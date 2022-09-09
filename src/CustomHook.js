b9600e3e9e8e030f3ba623b72077df1ea3f72f99
Robert Pliszka <robplisz@gmail.com>
2022-09-09 17:57:54 -0400

    Added comments to the custom hook for the form

--- src/CustomHook.js
+++ src/CustomHook.js
@@ -1,7 +1,9 @@
 import "./App.css";
 import { useState } from "react";
 
+// Created a custom hook named useInput that takes in one parameter initialValue
 function useInput(initialValue) {
+
   const [value, setValue] =
     useState(initialValue);
   return [
@@ -14,14 +16,18 @@ function useInput(initialValue) {
 }
 
 function FormCustom() {
+    // Calling the custom hook on the title and color property 
   const [titleProps, resetTitle] = useInput("");
   const [colorProps, resetColor] =
     useInput("#000000");
   const submit = (e) => {
+    // Prevents default browser refresh on form submit
     e.preventDefault();
+    // Alerts user with the information provided in the form
     alert(
       `${titleProps.value}, ${colorProps.value}`
     );
+    // Resets the values of the fields
     resetTitle();
     resetColor();
   };
